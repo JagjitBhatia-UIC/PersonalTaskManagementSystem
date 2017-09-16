a770d72f9c0afc028e6fec0a5822b722a5e9660a
JagjitBhatia-UIC <31589310+JagjitBhatia-UIC@users.noreply.github.com>
2017-09-16 14:22:24 -0500

    Updated PersonalTaskManagementSystem.java
    Replaced main input loop with an input thread (InputThread running InputHandler). Created TimerThread with Timer runnable. Integrated task time checking and notification.

--- Prototype/PersonalTaskManagementSystem.java
+++ Prototype/PersonalTaskManagementSystem.java
@@ -1,11 +1,14 @@
 /*
-  This is a command-line based prototype of the Personal Task Management System. The prototype is still under development 
-  but will incorporate the basic features of enabling a user to create a task (along with its respective date and time), complete 
+  This is a command-line based prototype of the Personal Task Management System. The prototype is still under development
+  but will incorporate the basic features of enabling a user to create a task (along with its respective date and time), complete
   a task (and input a rating of completeness), and end the current day which will result in a grade based on the completeness of
   the day's tasks.
-  
-  Next tasK: debug grade calculator (issue: always resulting to F)
-  Following task: threads for time checking and alerting user
+
+  As of 9/16/2017:
+
+  Next task: Test and clean up prototype
+  Following task: Start implementing Database for efficient storage
+  Looking into the future: Design GUI using JPanel and JFrame
 */
 
 
@@ -13,94 +16,128 @@
 import java.util.Scanner;
 import java.time.*;
 
-public class PersonalTaskManagementSystem
-{
-    public static void main(String[] args)
-    {
-        //Define and initialize all global variables: tasks (limit of 12), scanner, command, isComplete, and the counter
+public class PersonalTaskManagementSystem {
+    //Main method
+    public static void main(String[] args) {
+        //Initialize array of tasks (maximum 12), Scanner, and TaskCounter
         Task[] tasks = new Task[12];
         Scanner sc = new Scanner(System.in);
-        String command = "";
-        command = sc.nextLine();
-        boolean isComplete = false;
-        int count = 0;
-        
-        //Initiate command line input. User enters 'end' to exit program. 
-        while(!(command.equals("end")))
-        {
-            //'add task' function
-            if(command.equals("add task")){
-                System.out.println("Input task name");
-                String name = sc.nextLine();
-                System.out.println("Input task time");
-                String t = sc.nextLine();
-                int id = count;
-                LocalTime time = LocalTime.parse(t);    //User enters time in as a string (must be in hh:mm format); parsed into LocalTime
-                tasks[count] = new Task(id, name, time);    //Task number (count + 1) created (See Task class for reference)
-                System.out.println("task " + name + " created.");    //Confirmation to user
-                count++;    //Increment task counter 
-            }
-            
-            //'complete task' function
-            else if(command.equals("complete task"))
+        TaskCounter count = new TaskCounter();
+
+        //Define Runnable that InputThread will execute in background.
+        Runnable InputHandler = new Runnable() {
+            boolean isComplete = false; //Initialize end token
+
+            //Define run function (What will the thread actually do?)
+            public void run()
             {
-                Task t = new Task(-1,"null", LocalTime.MIDNIGHT);     //'null' task created; used to determine whether task t exists
-                System.out.println("Which task would you like to complete? (Enter name)");
-                String name = sc.nextLine();
-                //Search for task 'name'; assumes no duplicates in name
-                for(int i = 0; i<tasks.length; i++)
+                while (isComplete == false) //Make sure user is not done
                 {
-                   if((tasks[i].getName()).equals(name)){
-                        t = tasks[i];  //If task found, set task t to equal that task
-                        break;
+                    String command = "";  //Initialize command
+                    if (sc.hasNext()) {
+                        command = sc.nextLine(); //User input
                     }
 
+                    //'add task' function
+                    if (command.equals("add task")) {
+                        System.out.println("Input task name");
+                        String name = sc.nextLine();
+                        System.out.println("Input task time");
+                        String t = sc.nextLine();
+                        int id = count.getCounter();
+                        LocalTime time = LocalTime.parse(t);    //User enters time in as a string (must be in hh:mm format); parsed into LocalTime
+                        tasks[count.getCounter()] = new Task(id, name, time);    //Task number (count + 1) created (See Task class for reference)
+                        System.out.println("task " + name + " created.");    //Confirmation to user
+                        count.increment();    //Increment task counter
+                    }
+
+                    //'complete task' function
+                    else if (command.equals("complete task")) {
+                        Task t = new Task(-1, "null", LocalTime.MIDNIGHT);     //'null' task created; used to determine whether task t exists
+                        System.out.println("Which task would you like to complete? (Enter name)");
+                        String name = sc.nextLine();
+                        //Search for task 'name'; assumes no duplicates in name
+                        for (int i = 0; i < tasks.length; i++) {
+                            if ((tasks[i].getName()).equals(name)) {
+                                t = tasks[i];  //If task found, set task t to equal that task
+                                break;
+                            }
+                        }
+
+                        //If task t's id still equals -1, task 'name' doesn't exist
+                        if (t.getId() == -1) {
+                            System.out.println("Task not found");
+
+                        } else {
+                            System.out.println("Please rate the completeness of this task out of 10");  //User rates (him/her)self based on completeness of task
+                            String r = sc.nextLine();
+                            t.addRating(Integer.parseInt(r)); //Rating is inputted as a string and then parsed to integer.
+                        }
+
+                    }
+
+                    //end task function
+                    else if (command.equals("end day")) {
+                        int ratingTotal = 0;  //User's total score is initialized
+                        //Sum ratings of all tasks
+                        for (int i = 0; i < count.getCounter(); i++) {
+                            ratingTotal += tasks[i].getRating();
+                        }
+                        int totalPoints = count.getCounter() * 10;  //Since all ratings are out of 10, total # of possible points is 10 * (number of tasks)
+                        int percent = (ratingTotal / totalPoints) * 100;  //Compute score and convert into percentage
+                        String grade = "N/A";  //Initialize grade.
+                        //Determine grade based on percentage. Standard grading scale used.
+                        if ((percent >= 0) && (percent <= 59)) grade = "F";
+                        else if ((percent > 59) && (percent <= 69)) grade = "D";
+                        else if ((percent > 69) && (percent <= 79)) grade = "C";
+                        else if ((percent > 79) && (percent <= 89)) grade = "B";
+                        else if ((percent > 89) && (percent <= 100)) grade = "A";
+                        else System.out.println("Invalid Grade");   //If grade is somehow beyond range, inform user.
+                        System.out.println("All tasks completed. Your grade for today is: " + grade);
+                        isComplete = true;  //All tasks completed.
+                    }
+
+
+
                 }
-                
-                //If task t's id still equals -1, task 'name' doesn't exist
-                if(t.getId() == -1)
-                {
-                    System.out.println("Task not found");
-                    break;  //We don't want to proceed further if the task isn't found
-                }
-                System.out.println("Please rate the completeness of this task out of 10");  //User rates (him/her)self based on completeness of task
-                String r = sc.nextLine();
-                t.addRating(Integer.parseInt(r)); //Rating is inputted as a string and then parsed to integer. 
+
 
             }
-            
-            //end task function
-            else if(command.equals("end day"))
+        };
+
+        Runnable Timer = new Runnable()
+        {
+            public void run()
             {
-                int ratingTotal = 0;  //User's total score is initialized 
-                //Sum ratings of all tasks
-                for(int i = 0; i<count; i++)
+                while(true)
                 {
-                    ratingTotal += tasks[i].getRating(); 
-                }
-                int totalPoints = count * 10;  //Since all ratings are out of 10, total # of possible points is 10 * (number of tasks)
-                int percent = (ratingTotal/totalPoints) * 100;  //Compute score and convert into percentage
-                String grade = "N/A";  //Initialize grade.
-                //Determine grade based on percentage. Standard grading scale used. 
-                if((percent >= 0) && (percent <= 59)) grade = "F";
-                else if((percent > 59) && (percent <= 69)) grade = "D";
-                else if((percent > 69) && (percent <= 79)) grade = "C";
-                else if((percent > 79) && (percent <= 89)) grade = "B";
-                else if(percent > 89) && percent <= 100) grade = "A";
-                else System.out.println("Invalid Grade");   //If grade is somehow beyond range, inform user. 
-                System.out.println("All tasks completed. Your grade for today is: " + grade);
-                isComplete = true;  //All tasks completed.
-            }
-            //If end of day, break out of loop. 
-            if(isComplete == true){
-                command = "end";
-                break;
-            }
-            //If not end of day, proceed to next command. 
-            else{
-                command = sc.nextLine();
-            }
-        }
+                    int numTasks = count.getCounter();
+                    if (numTasks == 0) {
+                        System.out.println("Waiting for task......");
+                        try {
+                            Thread.sleep(60000);
+                        } catch (Exception e) {
+                            System.out.println("Interrupted");
+                        }
+                    }
 
+                    for (int taskQueue = 0; taskQueue < numTasks; taskQueue++) {
+                        if ((LocalTime.now().getHour() == tasks[taskQueue].getTime().getHour()) && (Math.abs(LocalTime.now().getMinute() - tasks[taskQueue].getTime().getMinute()) == 0)) {
+                            System.out.println("It's time for task '" + tasks[taskQueue].getName() + "' !");
+                            try {
+                                Thread.sleep(100000);
+                            } catch (Exception e) {
+                                System.out.println("Interrupted");
+                            }
+                        }
+                    }
+                }
+            }
+        };
+
+        Thread InputThread = new Thread(InputHandler);
+        Thread TimerThread = new Thread(Timer);
+        InputThread.start();
+        TimerThread.start();
     }
 }
